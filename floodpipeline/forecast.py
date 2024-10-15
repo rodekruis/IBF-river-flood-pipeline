7377bda0da489f5a377bcc07b81e83439a1b0fcb
pphung <45565029+p-phung@users.noreply.github.com>
2024-10-15 12:15:04 +0200

    recorrect UGA med threshold; fix scenario of disabling multi-thresholds

--- floodpipeline/forecast.py
+++ floodpipeline/forecast.py
@@ -111,7 +111,18 @@ def classify_alert(
             ):
                 alert_class = class_
     elif classify_alert_on == 'disable':
-        if triggered:
+        if (
+            triggered 
+            and type(alert_on_return_period) == dict
+        ):
+            alert_class = max(
+                alert_on_return_period, 
+                key=alert_on_return_period.get
+            )
+        elif (
+            triggered 
+            and type(alert_on_minimum_probability) == dict
+        ):
             alert_class = max(
                 alert_on_minimum_probability, 
                 key=alert_on_minimum_probability.get
